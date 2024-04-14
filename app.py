5043f4a9f10dd92324db672655ec4e2e9cb75178
apalai508 <shinyrayquazagx@gmail.com>
2024-04-13 19:30:39 -0700

    Final? Prompt update

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def ask_gpt(prompt):
                     "role": "user",
                     "content": "Here is a long text. Turn it into 3 twitter posts. IMPORTANT: You need to generate "
                                "some text in the voice of the author for each blog post to completely sum it up or "
-                               "finish off your point. If it's a blog, maybe not as much. Make sure not to simply split the text into 3 sections and summarize each one, rather make 3 short summarizations of the whole article. So each tweet should incoportate elements from throughout the article. ENSURE THAT ALL TEXT THAT IS GENERATED, IS GENERATED IN THE VOICE OF THE AUTHOR. Put in the format Post 1: Post, Post 2: Post , Post 3 :Post. IMPORTANT: NO SPECIAL CHARACTERS. Here's the text: " + prompt,
+                               "finish off your point. If it's a blog, maybe not as much. Make sure not to simply split the text into 3 sections and summarize each one, rather make 3 short summarizations of the whole article. So each tweet should incoportate elements from throughout the article. Each first sentence of each tweet (aka the hook) should be not in chronological order. So basically, I don't want you to write three individual tweets about three different things. I want you to write, for each tweet, an overview of the whole article. ENSURE THAT ALL TEXT THAT IS GENERATED, IS GENERATED IN THE VOICE OF THE AUTHOR. Put in the format Post 1: Post, Post 2: Post , Post 3 :Post. IMPORTANT: NO SPECIAL CHARACTERS. Here's the text: " + prompt,
                 }
             ],
             model="gpt-3.5-turbo",
